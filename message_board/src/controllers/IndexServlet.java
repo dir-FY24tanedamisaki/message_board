bc78e43173e2225415175646a406a24979144f47
misaki.taneda <misaki.taneda.j847@dirbato.co.jp>
2024-04-23 12:32:24 +0900

    フラッシュメッセージを追加（できず）

--- message_board/src/controllers/IndexServlet.java
+++ message_board/src/controllers/IndexServlet.java
@@ -11,25 +11,15 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import models.Message;
 import utils.DBUtil;
- 
-/**
-* Servlet implementation class IndexServlet
-*/
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
         private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public IndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+        super();       
     }
  
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
@@ -39,6 +29,12 @@ public class IndexServlet extends HttpServlet {
         em.close();
 
         request.setAttribute("messages", messages);
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         var rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
